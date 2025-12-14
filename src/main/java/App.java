302138e312c36ac542269041ab0715eb9a8e6b57
L00196713 <L00196713@atu.ie>
2025-12-14 20:03:41 +0000

    Update dependency checker and add new dependency to check

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,25 @@
 package com.cicd.simple;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * A simple application class for CI/CD demonstration.
  */
 public class App {
     
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     /**
      * A simple method to demonstrate functionality.
      * @return a fixed greeting string
      */
     public String getGreeting() {
+        logger.info("getGreeting is returning Hello World!");
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        logger.debug("Application starting up.");
         System.out.println(new App().getGreeting());
+        logger.warn("Application stopping.");
     }
 }
